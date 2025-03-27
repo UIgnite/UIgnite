d37edce5799e103f504d2c2bc8cdf488c2f0b19c
Aditya Sharma <aadisharma.in+github@gmail.com>
2025-03-27 18:49:24 +0530

    feat: update Toast component positioning and enhance toast display logic

--- src/hooks/toast.jsx
+++ src/hooks/toast.jsx
@@ -1,35 +1,45 @@
 import Toast from "../components/Toast";
 
-import { useCallback, useContext, createContext, useState, useRef } from "react";
+import {
+  useCallback,
+  useContext,
+  createContext,
+  useState,
+  useRef,
+} from "react";
 
 const ToastContext = createContext(null);
 
 const ToastProvider = ({ children }) => {
+  const toastRef = useRef(null);
 
-  const toastRef = useRef(null)
-
-  const [title, setTitle] = useState('Toast is here')
+  const [title, setTitle] = useState("Toast is here");
   const [options, setOptions] = useState({
     variant: "default",
     desc: null,
-    timeout: 3000
-  })
+    timeout: 3000,
+  });
 
-  const handleShowToast = useCallback((ttle, opt) => {
-    setTitle(ttle)
-    setOptions(options=>({...options, ...opt}))
-    if(toastRef.current){
-      toastRef.current.removeAttribute('hidden')
-      setTimeout(()=>{
-        toastRef.current.setAttribute('hidden', true);
-      }, options.timeout)
-    }
-  }, [setTitle, setOptions, toastRef, options, title]);
+  const handleShowToast = useCallback(
+    (ttle, opt) => {
+      setTitle(ttle);
+      setOptions((options) => ({ ...options, ...opt }));
+      if (toastRef.current) {
+        if (toastRef.current.classList.contains("-bottom-20")) {
+          toastRef.current.classList.replace("-bottom-20", "bottom-10");
+          setTimeout(() => {
+            toastRef.current.classList.replace("bottom-10", "-bottom-20");
+          }, options.timeout);
+        }
+      }
+    },
+    [setTitle, setOptions, toastRef, options, title]
+  );
 
   return (
     <ToastContext.Provider value={{ showToast: handleShowToast }}>
       {children}
-      <Toast ref={toastRef} title={title} options={options}/>
+      <Toast ref={toastRef} title={title} options={options} />
     </ToastContext.Provider>
   );
 };
