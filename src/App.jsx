c4789f257a4578cb528b07fa1543cb75060a3e93
Aditya Sharma <aadisharma.in+github@gmail.com>
2025-03-27 17:11:02 +0530

    Testing purposes only

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
-
-
 function App() {
- 
-
   return (
     <>
      <div className="flex flex-col items-center justify-center h-screen bg-gray-900 text-white text-center">
       <h1 className="text-6xl font-bold tracking-wide text-orange-500 ">
         UIgnite 🔥
       </h1>
-      <p className="text-2xl mt-4 text-gray-500">Coming Soon...</p>
+      <p className="text-2xl mt-4 text-gray-500">Coming Soon......</p>
     </div>
     </>
   )
