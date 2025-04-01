950fcacaf8a94db93a7dc10228f8207e8ae49d04
Aman Kumar Gupta <akgbytes@gmail.com>
2025-04-01 11:27:03 +0530

    feat: add avatar component

--- src/pages/Components.jsx
+++ src/pages/Components.jsx
@@ -1,29 +1,35 @@
-import React from 'react'
-import { Button } from '../components/Button'
-import { Tooltip } from '../components/Tooltip'
-import { useToast } from '../hooks/toast'
+import React from "react";
+import { Button } from "../components/Button";
+import { Tooltip } from "../components/Tooltip";
+import { useToast } from "../hooks/toast";
+import Avatar from "../components/Avatar";
 
 const Components = () => {
-    const toast = useToast()
+  const toast = useToast();
   return (
     <>
-         <div className="flex flex-col items-center justify-center h-screen  text-center">
-         
-          <Button onClick={()=>{
+      <div className="flex flex-col items-center justify-center h-screen  text-center bg-zinc-700">
+        <Button
+          onClick={() => {
             toast.showToast("New toast here", {
               desc: "I am the new desc.",
-              timeout: 3000
-            })
-          }} className="mt-5 bg-gray-50 text-black hover:bg-gray-100">Browse Components</Button>
-        
-          <Tooltip className="" title={"I am Tooltip "} options={ { delay : 300 }} > 
-            <Button>Hover me</Button>
-          </Tooltip>
-          
-         </div>
-        
-        </>
-  )
-}
+              timeout: 3000,
+            });
+          }}
+          className="mt-5 bg-gray-50 text-black hover:bg-gray-100"
+        >
+          Browse Components
+        </Button>
 
-export default Components
+        <Tooltip className="" title={"I am Tooltip "} options={{ delay: 300 }}>
+          <Button>Hover me</Button>
+        </Tooltip>
+
+        <Avatar url={"piyush-sir.png"} alt={"user"} />
+        <Avatar url={"hitesh-sir.png"} alt={"user"} />
+      </div>
+    </>
+  );
+};
+
+export default Components;
