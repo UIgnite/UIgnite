86895969c0d6ea9f3f467dbef3b54972e0435ac3
rambhardwajj <rambhardwaj101000@gmail.com>
2025-04-02 00:34:12 +0530

    feat: Accordion Wrapper with dynamic closing added

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -2,25 +2,19 @@ import React from 'react'
 import { useState } from 'react'
 import { cn } from '../utils/lib'
 
-const Accordion = ({title, content, className="", defaultOpen = false, open=false}) => {
+const Accordion = ({title, content, className="", defaultOpen = false, open=false, isOpen=null, onToggle }) => {
   const [visible, setVisible] = useState(defaultOpen)
   const toggleAccordion = () =>{
-    if( visible){
-      open = false
-      setVisible(false)
-    }else{
-      open = true;
-      setVisible(true)
-    }
+    setVisible(!visible)
   }
 
   return (
     <>
     <div>
-      <div onClick={toggleAccordion} className={cn('cursor-pointer bg-gray-800  hover:bg-gray-900 flex  items-center justify-between min-w-2xs  min-h-md ease-in-out')}>
+      <div onClick={onToggle ?? toggleAccordion} className={cn('cursor-pointer bg-gray-800  hover:bg-gray-900 flex  items-center justify-between min-w-2xs  min-h-md ease-in-out')}>
         <div className='ml-2 px-2 py-2 text-white'> {title} </div> 
         <svg className={`mr-2 transform transition-transform duration-300 
-          ${visible ? "rotate-180" : "rotate-0"}`}
+          ${(isOpen ?? visible) ? "rotate-180" : "rotate-0"}`}
           width="20"
           height="15"
           viewBox="0 0 24 24"
@@ -36,13 +30,13 @@ const Accordion = ({title, content, className="", defaultOpen = false, open=fals
         </div>      
         <div
           className={` overflow-hidden transition-all duration-300 ease-in-out hover:bg-gray-800 
-        ${visible ? "max-h-40 opacity-100 translate-y-0" : "max-h-0 opacity-0 -translate-y-5"}`}
+        ${(isOpen ?? visible) ? "max-h-40 opacity-100 translate-y-0" : "max-h-0 opacity-0 -translate-y-5"}`}
       >
         <div className="bg-gray-800 text-left -z-1 px-4 py-1 text-sm text-white">{content}</div>
       </div>
 
-      <hr className='w-m h-px  bg-gray-200 border-0 ' />
-    {/* <div className='flex h-14 items-center justify-between border-b border-zinc-200 ' > </div> */}
+      <hr className='w-m h-px  bg-gray-500 border-0 ' />
+      {/* <div className='flex h-14 items-center justify-between border-b border-zinc-200 ' > </div> */}
       
     </div>
     </>
