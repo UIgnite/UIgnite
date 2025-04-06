6be63d5a03bf165c73bfcb3218ededc696f82706
Saurav Jha <srvtj629@gmail.com>
2025-04-06 17:46:11 +0530

    Feat: Add Skeleton component and integrate it into Components page; update Spinner for theme support

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { cn } from "../utils/lib";
+import {useTheme} from '../hooks/theme';
 export const Spinner = ({ className = "" }) => {
+  const {theme} = useTheme();
   return (
-    <div class={cn("flex space-x-2 justify-center items-center dark:invert",className)}>
-	<div class='h-3 w-3 bg-black rounded-full animate-bounce [animation-delay:-0.3s]'></div>
-	<div class='h-3 w-3 bg-black rounded-full animate-bounce [animation-delay:-0.15s]'></div>
-	<div class='h-3 w-3 bg-black rounded-full animate-bounce'></div>
+    <div class={cn("flex space-x-2 justify-center items-center ",className)}>
+	<div class={`h-3 w-3 ${theme==="dark"?"bg-white":"bg-black"} rounded-full animate-bounce [animation-delay:-0.3s]`}></div>
+	<div class={`h-3 w-3 ${theme==="dark"?"bg-white":"bg-black"}  rounded-full animate-bounce [animation-delay:-0.15s]`}></div>
+	<div class={`h-3 w-3 ${theme==="dark"?"bg-white":"bg-black"} rounded-full animate-bounce`}></div>
 </div>
   );
 };
