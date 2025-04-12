832a698f7a010b4df8f745e9e1ae26c277ae388f
Saurav Jha <srvtj629@gmail.com>
2025-04-13 03:16:36 +0530

    button spinner skeleton pricing dropdown components migrated to tsx

---
+++ packages/uignite/src/components/Dropdown.tsx
@@ -0,0 +1,44 @@
+import React, {useCallback, useRef, useState} from 'react';
+import {Button} from './Button';
+
+export default function Dropdown({
+  children,
+  button,
+  title = 'Open',
+}: React.PropsWithChildren<{button: React.ReactNode; title?: string}>) {
+  const buttonRef = useRef<HTMLDivElement | null>(null);
+  const dropDownRef = useRef(null);
+
+  const [open, setOpen] = useState(false);
+
+  const handleToogleOpen = useCallback(() => {
+    setOpen(!open);
+  }, [open, setOpen]);
+
+  return (
+    <React.Fragment>
+      {open ? (
+        <span
+          onClick={handleToogleOpen}
+          className="fixed z-10 bg-black/20 inset-0"
+        ></span>
+      ) : null}
+      <div>
+        {open ? (
+          <div
+            ref={dropDownRef}
+            style={{
+              top: (buttonRef.current?.getBoundingClientRect()?.top ?? 0) + 50,
+            }}
+            className="min-w-56 fixed z-20 bg-neutral-200 min-h-5 rounded-lg border border-neutral-400 shadow p-1 flex flex-col gap-y-1"
+          >
+            {children}
+          </div>
+        ) : null}
+        <div ref={buttonRef} role="button" onClick={handleToogleOpen}>
+          {button ?? <Button>{title}</Button>}
+        </div>
+      </div>
+    </React.Fragment>
+  );
+}
---
+++ packages/uignite/src/components/Spinner.tsx
@@ -0,0 +1,23 @@
+
+import { cn } from "../utils/lib";
+
+interface SpinnerProps {
+  className?: string;
+}
+
+export const Spinner = ({ className = "" }: SpinnerProps) => {
+
+  return (
+    <div className={cn("flex space-x-2 justify-center items-center ", className)}>
+      <div
+        className={`h-3 w-3  rounded-full animate-bounce [animation-delay:-0.3s]`}
+      ></div>
+      <div
+        className={`h-3 w-3   rounded-full animate-bounce [animation-delay:-0.15s]`}
+      ></div>
+      <div
+        className={`h-3 w-3 rounded-full animate-bounce`}
+      ></div>
+    </div>
+  );
+};
