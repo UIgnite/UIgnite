53e4bfb627dc1c8dc65fcf4e869d2b2df321cda5
Aman Kumar Gupta <akgbytes@gmail.com>
2025-04-13 00:32:19 +0530

    feat: add btn

--- packages/uignite/src/components/Avatar.tsx
+++ packages/uignite/src/components/Avatar.tsx
@@ -1,15 +1,21 @@
-// import {cn} from '../utils/lib';
+import {cn} from '../utils/lib';
 
-// const Avatar = ({url, alt, className = '', ...props}) => {
-//   return (
-//     <div className="flex gap-4 mt-2">
-//       <img
-//         className={cn(`rounded-full h-12 w-12 object-cover`, className)}
-//         src={url}
-//         alt={alt}
-//       />
-//     </div>
-//   );
-// };
+interface AvatarParamsT {
+  url: string;
+  alt: string;
+  className?: string;
+}
 
-// export default Avatar;
+const Avatar = ({url, alt, className = ''}: AvatarParamsT) => {
+  return (
+    <div className="flex gap-4 mt-2">
+      <img
+        className={cn(`rounded-full h-12 w-12 object-cover`, className)}
+        src={url}
+        alt={alt}
+      />
+    </div>
+  );
+};
+
+export default Avatar;
