d78d802fc8bab5e88a040ff25ef7cd6ae6f379b4
Aman Kumar Gupta <akgbytes@gmail.com>
2025-04-13 00:18:01 +0530

    feat: add util fn

---
+++ packages/uignite/src/components/Avatar.tsx
@@ -0,0 +1,15 @@
+// import {cn} from '../utils/lib';
+
+// const Avatar = ({url, alt, className = '', ...props}) => {
+//   return (
+//     <div className="flex gap-4 mt-2">
+//       <img
+//         className={cn(`rounded-full h-12 w-12 object-cover`, className)}
+//         src={url}
+//         alt={alt}
+//       />
+//     </div>
+//   );
+// };
+
+// export default Avatar;
--- packages/uignite/vite.config.ts
+++ packages/uignite/vite.config.ts
@@ -1,19 +1,22 @@
-import fs from 'fs'
+import fs from 'fs';
 import path from 'path';
 import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
-
 function getComponentEntries(dir: string) {
   const files = fs.readdirSync(dir);
   const entries: Record<string, string> = {};
 
-  files.forEach(file => {
+  files.forEach((file) => {
     const fullPath = path.resolve(dir, file);
     const stat = fs.statSync(fullPath);
 
-    if (stat.isFile() && /\.(ts|tsx)$/.test(file) && !file.startsWith('index')) {
+    if (
+      stat.isFile() &&
+      /\.(ts|tsx)$/.test(file) &&
+      !file.startsWith('index')
+    ) {
       const name = path.parse(file).name;
       entries[`components/${name}`] = fullPath;
     }
@@ -21,7 +24,6 @@ function getComponentEntries(dir: string) {
 
   return entries;
 }
-
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -35,7 +37,7 @@ export default defineConfig({
     lib: {
       entry: getComponentEntries(path.resolve(__dirname, 'src/components')),
       fileName: (format, entryName) => `${entryName}.${format}.js`,
-      formats: ['es', 'cjs']
+      formats: ['es', 'cjs'],
     },
     rollupOptions: {
       external: ['react', 'react-dom'],
@@ -44,7 +46,7 @@ export default defineConfig({
         preserveModules: true,
         preserveModulesRoot: 'src',
         dir: 'dist',
-      }
-    }
-  }
+      },
+    },
+  },
 });
