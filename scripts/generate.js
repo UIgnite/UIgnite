0295a7136880856204dc9adca8c82e05fcf09fbd
Aditya Sharma <aadisharma.in+github@gmail.com>
2025-04-15 01:59:35 +0530

    Refactor generate.js: Add async functions for reading and writing component files

--- scripts/generate.js
+++ scripts/generate.js
@@ -1,7 +1,53 @@
 import fs from 'fs/promises';
+import pth from "path"
+import registoryConfig from '../registry.json' with {type: 'json'};
 
-(async () => {
+async function readComponentFile(path) {
   try {
+    const content = await fs.readFile(path, 'utf-8');
+    return content;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function writeComponentContent(name, content) {
+  try {
+    const path = pth.join(content[''],`${name}.json`)
+    await fs.writeFile()
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+(() => {
+  try {
+    const items = registoryConfig['items'];
+    for (let index = 0; index < items.length; index++) {
+      const element = items[index];
+      const elementSchemaJson = {
+        $schema: 'https://ui.shadcn.com/schema/registry-item.json',
+        name: element['name'],
+        type: element['type'],
+        dependencies: element['dependencies'],
+        files: element['files'].map(async (value) => {
+          const content = await readComponentFile(value['path']);
+          if (!content)
+            return console.log(
+              'Something went wrong during reading file content'
+            );
+          return {
+            content,
+            type: value['type'],
+            path: value['path'],
+          };
+        }),
+      };
+      element['files'].forEach(async (file, idx)=>{
+        '/'
+        await writeComponentContent();
+      })
+    }
   } catch (error) {
     console.error(`Error occured: ${error.message}`);
   }
