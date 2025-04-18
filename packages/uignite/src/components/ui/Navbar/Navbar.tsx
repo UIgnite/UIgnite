39fbbb7a5d597c7ee21de5392d5fc86835abda8b
Aman Kumar Gupta <akgbytes@gmail.com>
2025-04-18 16:31:11 +0530

    Refactor Navbar and TestCards components for improved theme handling and layout

--- packages/uignite/src/components/ui/Navbar/Navbar.tsx
+++ packages/uignite/src/components/ui/Navbar/Navbar.tsx
@@ -1,52 +1,64 @@
-import {GitHubLogoIcon} from '@radix-ui/react-icons';
 import {ThemeToggleBtn} from '@/lib/ThemeToggleBtn';
+import {useTheme} from '@/hooks';
+import {Button} from '../Button';
 
 const Navbar = () => {
+  const {theme} = useTheme();
   return (
-    <div className="relative  max-w-[88rem] px-4 sm:px-6 lg:px-8 flex flex-col w-full py-5 mx-auto md:flex-row md:items-center md:justify-between">
-      {/* logo */}
-      <div className="flex flex-row gap-12 items-center justify-between text-[1rem] lg:justify-start">
-        <img src="/favicon.png" className="w-8 object-contain" alt="Logo" />
-        <h4 className="text-2xl font-semibold -mt-1">
-          UI<span className="text-primary">gnite</span>
-        </h4>
+    <div>
+      <div className="my-24 border"></div>
 
-        {/* nav links */}
-        <nav className="flex justify-end space-x-2">
-          <a
-            href="/pricing"
-            className="cursor-pointer p-2 rounded opacity-70 hover:opacity-100"
-          >
-            <p>Docs</p>
-          </a>
-          <a
-            href="/components"
-            className="cursor-pointer p-2 rounded opacity-70 hover:opacity-100"
-          >
-            <p>Components</p>
-          </a>
-          <a
-            href="/"
-            className="cursor-pointer p-2 rounded opacity-70 hover:opacity-100"
-          >
-            <p>About</p>
-          </a>
-        </nav>
-      </div>
+      <div
+        className="relative px-4 sm:px-6 lg:px-8 flex py-1 mx-auto flex-row items-center justify-between
+    h-14 top-0 w-full border-b  border-gray-200
+    "
+      >
+        {/* logo */}
+        <div className="flex flex-row gap-12 items-center justify-between text-[1rem] lg:justify-start">
+          <img
+            src={`${theme === 'light' ? '/chaicode-black.png' : '/chaicode-white.png'}`}
+            className="size-32 object-contain"
+            alt="Logo"
+          />
 
-      {/* github and mode switch */}
-      <div className="hidden md:flex md:justify-center md:items-center md:gap-1">
-        {/* md:justify-end md:space-x-4 */}
-        <ThemeToggleBtn />
-        <a
-          href="https://github.com/UIgnite/UIgnite"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <div className="!dark cursor-pointer p-2 rounded hover:bg-zinc-200 dark:hover:bg-zinc-700">
-            <GitHubLogoIcon className=" h-4 w-4 " />
+          {/* nav links */}
+          <nav className="hidden sm:flex sm:justify-end sm:space-x-2">
+            <a
+              href="#"
+              className="cursor-pointer p-2 rounded opacity-70 hover:opacity-100"
+            >
+              <p>Cohorts</p>
+            </a>
+            <a
+              href="#"
+              className="cursor-pointer p-2 rounded opacity-70 hover:opacity-100"
+            >
+              <p>Udemy</p>
+            </a>
+            <a
+              href="#"
+              className="cursor-pointer p-2 rounded opacity-70 hover:opacity-100"
+            >
+              <p>Docs</p>
+            </a>
+            <a
+              href="#"
+              className="cursor-pointer p-2 rounded opacity-70 hover:opacity-100"
+            >
+              <p>Reviews</p>
+            </a>
+          </nav>
+        </div>
+
+        {/* github and mode switch */}
+        <div className="flex justify-center items-center gap-1">
+          <div className="">
+            <ThemeToggleBtn />
           </div>
-        </a>
+          <Button className="ml-2 size-9 bg-orange-500 hover:bg-orange-600">
+            Login
+          </Button>
+        </div>
       </div>
     </div>
   );
