a8def53c1686d32be49ba7621123a5e1df650014
Aman Kumar Gupta <akgbytes@gmail.com>
2025-04-20 01:49:14 +0530

    Refactor Footer component to use props for dynamic content and improve structure

--- packages/uignite/src/components/ui/Footer/Footer.tsx
+++ packages/uignite/src/components/ui/Footer/Footer.tsx
@@ -1,168 +1,118 @@
-import {FiGithub, FiLinkedin} from 'react-icons/fi';
-import {FaDiscord, FaInstagram} from 'react-icons/fa';
-import {FaXTwitter} from 'react-icons/fa6';
-import {SlSocialYoutube} from 'react-icons/sl';
-import {useTheme} from '@/hooks/theme';
+import {cn} from '@/lib/utils';
+import {useTheme} from '@pkgs/uignite';
+import {JSX} from 'react';
 
-const Footer = () => {
+export interface FooterProps {
+  logoDark: string;
+  logoLight: string;
+  tagline: string;
+  socialLinks: {
+    icon: JSX.Element;
+    href: string;
+    label: string;
+  }[];
+  socialLinksStyle?: string;
+  linkSections: {
+    title: string;
+    links: {
+      name: string;
+      href: string;
+    }[];
+  }[];
+  linkStyle?: string;
+  copyrightText: string;
+  builtByText: string;
+  className?: string;
+}
+
+const Footer = ({
+  logoDark,
+  logoLight,
+  tagline,
+  socialLinks,
+  socialLinksStyle,
+  linkSections,
+  linkStyle,
+  copyrightText,
+  builtByText,
+  className = '',
+}: FooterProps) => {
   const {theme} = useTheme();
+
   return (
-    <div className="w-full h-full">
-      <div className="dark:border-t-zinc-800 border-t mt-6 border-t-zinc-300"></div>
-      <div className="max-w-[1200px] flex flex-col w-full py-5 justify-between mx-auto pb-5">
-        <div className="mt-4 flex justify-between">
-          {/* website */}
-          <div className="flex flex-col gap-6">
-            {/* logo */}
-            <div className="w-56 h-12 transition-all duration-300 ease-in-out hover:scale-105  cursor-pointer -ml-4">
-              <img
-                alt="UIgnite"
-                className="h-full w-full"
-                src={
-                  theme === 'dark'
-                    ? '/chaicode-white.png'
-                    : '/chaicode-black.png'
-                }
-              />
-            </div>
-
-            {/* tagline */}
-            <p className="text-gray-600 dark:text-zinc-400 w-72">
-              Home for programmers
-            </p>
-
-            {/* socials */}
-            <div className="flex gap-4 text-zinc-500 ">
-              <a href="https://github.com/hiteshchoudhary" target="_blank">
-                <FiGithub className="transition-all duration-200 ease-in-out hover:scale-105 dark:hover:text-orange-500 hover:text-zinc-700 size-5 cursor-pointer" />
-              </a>
-
-              <a href="https://discord.com/invite/WDrH3zuWFb" target="_blank">
-                <FaDiscord className="transition-all duration-200 ease-in-out hover:scale-105 dark:hover:text-orange-500 hover:text-zinc-700 size-5 cursor-pointer" />
-              </a>
-
-              <a href="https://x.com/hiteshdotcom" target="_blank">
-                <FaXTwitter className="transition-all duration-200 ease-in-out hover:scale-105 dark:hover:text-orange-500 hover:text-zinc-700 size-5 cursor-pointer" />
-              </a>
-
-              <a
-                href="https://www.linkedin.com/in/hiteshchoudhary"
-                target="_blank"
-              >
-                <FiLinkedin className="transition-all duration-200 ease-in-out hover:scale-105 dark:hover:text-orange-500 hover:text-zinc-700 size-5 cursor-pointer" />
-              </a>
-
-              <a
-                href="https://www.instagram.com/hiteshchoudharyofficial"
-                target="_blank"
-              >
-                <FaInstagram className="transition-all duration-200 ease-in-out hover:scale-105 dark:hover:text-orange-500 hover:text-zinc-700 size-5 cursor-pointer" />
-              </a>
-
-              <a href="https://www.youtube.com/@chaiaurcode" target="_blank">
-                <SlSocialYoutube className="transition-all duration-200 ease-in-out hover:scale-105 dark:hover:text-orange-500 hover:text-zinc-700 size-5 cursor-pointer" />
-              </a>
-            </div>
+    <footer
+      className={`w-full px-4 sm:px-8 md:px-10 py-6 dark:border-t-zinc-800 border-t border-t-zinc-300 ${className}`}
+    >
+      <div className="max-w-7xl mx-auto py-8 flex flex-col gap-10 md:flex-row md:justify-between">
+        {/* Left Section */}
+        <div className="flex flex-col gap-6 max-w-md">
+          <div className="w-48 h-12 hover:scale-105 transition-all cursor-pointer">
+            <img
+              alt="Logo"
+              className="h-full w-full object-contain"
+              src={theme === 'dark' ? logoDark : logoLight}
+            />
           </div>
 
-          {/* other sections like product getStarted legal */}
-          <div className="flex gap-16 mt-3">
-            <div className="text-zinc-500 dark:text-zinc-400 flex flex-col gap-2">
-              <h2 className="text-zinc-800 dark:text-zinc-100 font-semibold">
-                Product
-              </h2>
-              <a
-                href="https://courses.chaicode.com/learn"
-                target="_blank"
-                className="cursor-pointer dark:hover:text-orange-400 hover:text-zinc-700"
-              >
-                Courses
-              </a>
-              <a
-                href="https://courses.chaicode.com/learn/view-all?show=batch&type=17"
-                target="_blank"
-                className="cursor-pointer dark:hover:text-orange-400 hover:text-zinc-700"
-              >
-                Cohort
-              </a>
-              <a
-                href="https://courses.chaicode.com/learn/batch/about?bundleId=226894"
-                target="_blank"
-                className="cursor-pointer dark:hover:text-orange-400 hover:text-zinc-700"
-              >
-                Coding Hero
-              </a>
-              <a
-                href="https://masterji.co/login"
-                target="_blank"
-                className="cursor-pointer dark:hover:text-orange-400 hover:text-zinc-700"
-              >
-                MasterJI
-              </a>
-            </div>
-            <div className="text-zinc-500 dark:text-zinc-400 flex flex-col gap-2">
-              <h2 className="text-zinc-800 dark:text-zinc-100 font-semibold">
-                Resources
-              </h2>
+          <p className="text-sm text-gray-600 dark:text-zinc-400">{tagline}</p>
 
+          {/* social links */}
+          <div className="flex gap-4 text-zinc-500">
+            {socialLinks.map(({icon, href, label}) => (
               <a
-                href="https://freeapi.app/"
+                key={label}
+                href={href}
                 target="_blank"
-                className="cursor-pointer dark:hover:text-orange-400 hover:text-zinc-700"
+                aria-label={label}
+                rel="noopener noreferrer"
+                className={cn(
+                  'hover:text-zinc-800 dark:hover:text-zinc-100 hover:scale-105 transition-all',
+                  socialLinksStyle
+                )}
               >
-                FreeAPI
+                {icon}
               </a>
-              <a
-                href="https://chaidocs.vercel.app/"
-                target="_blank"
-                className="cursor-pointer dark:hover:text-orange-400 hover:text-zinc-700"
-              >
-                ChaiDocs
-              </a>
-            </div>
-
-            <div className="text-zinc-500 dark:text-zinc-400 flex flex-col gap-2">
-              <h2 className="text-zinc-800 dark:text-zinc-100 font-semibold">
-                Legal
-              </h2>
-              <a
-                href="#"
-                className="cursor-pointer dark:hover:text-orange-400 hover:text-zinc-700"
-              >
-                Terms of Service
-              </a>
-              <a
-                href="#"
-                className="cursor-pointer dark:hover:text-orange-400 hover:text-zinc-700"
-              >
-                Privacy Policy
-              </a>
-              <a
-                href="#"
-                className="cursor-pointer dark:hover:text-orange-400 hover:text-zinc-700"
-              >
-                Pricing Policy
-              </a>
-              <a
-                href="#"
-                className="cursor-pointer dark:hover:text-orange-400 hover:text-zinc-700"
-              >
-                Refund Policy
-              </a>
-            </div>
+            ))}
           </div>
         </div>
 
-        <div className="dark:border-t-zinc-800 border-t border-t-zinc-300 mt-8 mb-6"></div>
-        <div className="flex justify-between text-zinc-800 dark:text-zinc-300">
-          <div className="">© 2025 ChaiCode. All rights reserved.</div>
+        {/* Right Section */}
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10 text-sm text-zinc-500 dark:text-zinc-400">
+          {linkSections.map(({title, links}) => (
+            <div key={title} className="flex flex-col gap-2">
+              <h3 className="text-zinc-800 dark:text-zinc-100 font-semibold">
+                {title}
+              </h3>
+              {/* links */}
+              {links.map(({name, href}) => (
+                <a
+                  key={name}
+                  href={href}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className={cn(
+                    'hover:text-zinc-700 dark:hover:text-zinc-100',
+                    linkStyle
+                  )}
+                >
+                  {name}
+                </a>
+              ))}
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="dark:border-t-zinc-800 border-t border-t-zinc-300 mt-8 pt-6">
+        <div className="max-w-7xl mx-auto flex flex-col md:flex-row justify-between text-sm text-zinc-800 dark:text-zinc-300 gap-3">
+          <div>© 2025 {copyrightText}. All rights reserved.</div>
           <div>
-            Built with ❤️ by{' '}
-            <span className="underline cursor-pointer">ChaiCode</span>
+            <span>Built with ❤️ by </span>{' '}
+            <span className="underline cursor-pointer">{builtByText}</span>
           </div>
         </div>
       </div>
-    </div>
+    </footer>
   );
 };
 
