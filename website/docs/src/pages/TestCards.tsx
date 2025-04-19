45a25b3a27759ca34ceb11fb593281fbd8a0174d
Aman Kumar Gupta <akgbytes@gmail.com>
2025-04-19 13:05:51 +0530

    Uncomment and integrate TestCards component into App; enhance TestCards layout and functionality

--- website/docs/src/pages/TestCards.tsx
+++ website/docs/src/pages/TestCards.tsx
@@ -1,60 +1,112 @@
-// import { XMarkIcon } from '@heroicons/react/20/solid'
+import {
+  Avatar,
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+  Button,
+  cn,
+} from '@pkgs/uignite';
+import {useState} from 'react';
 
-export default function Example() {
+function TestCards() {
+  const [status, setStatus] = useState('Follow');
+
+  function clickHandler() {
+    const final = status === 'Follow' ? 'Unfollow' : 'Follow';
+    setStatus(final);
+  }
+
   return (
-    <div className="relative isolate flex items-center gap-x-6 overflow-hidden bg-gray-50 px-6 py-2.5 sm:px-3.5 sm:before:flex-1">
-      <div
-        aria-hidden="true"
-        className="absolute top-1/2 left-[max(-7rem,calc(50%-52rem))] -z-10 -translate-y-1/2 transform-gpu blur-2xl"
-      >
-        <div
-          style={{
-            clipPath:
-              'polygon(74.8% 41.9%, 97.2% 73.2%, 100% 34.9%, 92.5% 0.4%, 87.5% 0%, 75% 28.6%, 58.5% 54.6%, 50.1% 56.8%, 46.9% 44%, 48.3% 17.4%, 24.7% 53.9%, 0% 27.9%, 11.9% 74.2%, 24.9% 54.1%, 68.6% 100%, 74.8% 41.9%)',
-          }}
-          className="aspect-577/310 w-[36.0625rem] bg-linear-to-r from-[#ff80b5] to-[#9089fc] opacity-30"
-        />
-      </div>
-      <div
-        aria-hidden="true"
-        className="absolute top-1/2 left-[max(45rem,calc(50%+8rem))] -z-10 -translate-y-1/2 transform-gpu blur-2xl"
-      >
-        <div
-          style={{
-            clipPath:
-              'polygon(74.8% 41.9%, 97.2% 73.2%, 100% 34.9%, 92.5% 0.4%, 87.5% 0%, 75% 28.6%, 58.5% 54.6%, 50.1% 56.8%, 46.9% 44%, 48.3% 17.4%, 24.7% 53.9%, 0% 27.9%, 11.9% 74.2%, 24.9% 54.1%, 68.6% 100%, 74.8% 41.9%)',
-          }}
-          className="aspect-577/310 w-[36.0625rem] bg-linear-to-r from-[#ff80b5] to-[#9089fc] opacity-30"
-        />
-      </div>
-      <div className="flex flex-wrap items-center gap-x-4 gap-y-2">
-        <p className="text-sm/6 text-gray-900">
-          <strong className="font-semibold">GeneriCon 2023</strong>
-          <svg
-            viewBox="0 0 2 2"
-            aria-hidden="true"
-            className="mx-2 inline size-0.5 fill-current"
-          >
-            <circle r={1} cx={1} cy={1} />
-          </svg>
-          Join us in Denver from June 7 – 9 to see what’s coming next.
-        </p>
-        <a
-          href="#"
-          className="flex-none rounded-full bg-gray-900 px-3.5 py-1 text-sm font-semibold text-white shadow-xs hover:bg-gray-700 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900"
-        >
-          Register now <span aria-hidden="true">&rarr;</span>
-        </a>
-      </div>
-      <div className="flex flex-1 justify-end">
-        <button
-          type="button"
-          className="-m-3 p-3 focus-visible:outline-offset-[-4px]"
-        >
-          <span className="sr-only">Dismiss</span>
-          {/* <XMarkIcon aria-hidden="true" className="size-5 text-gray-900" /> */}
-        </button>
+    <div className="flex justify-center items-center h-full w-full py-36">
+      <div>
+        <Card className="mt-6 rounded-2xl shadow-lg max-h-min">
+          <CardHeader className="flex flex-row justify-between px-4 pt-2 items-center">
+            <div className="flex gap-3 items-center">
+              <Avatar url="/hitesh-sir.png" alt="avatar" />
+              <div>
+                <CardTitle className="text-base">Hitesh Chaudhary</CardTitle>
+                <CardDescription className="text-sm dark:text-gray-400">
+                  @HiteshDotCom
+                </CardDescription>
+              </div>
+            </div>
+            <Button
+              onClick={clickHandler}
+              className={`${
+                status === 'Unfollow'
+                  ? 'bg-zinc-900 hover:bg-neutral-900 dark:border dark:border-white border-2'
+                  : 'bg-blue-600 hover:bg-blue-700'
+              } transform transition-all duration-200 ease-out active:scale-95 cursor-pointer text-white text-sm font-medium py-1.5 px-4 rounded-full`}
+            >
+              {status}
+            </Button>
+          </CardHeader>
+
+          <CardContent className="text-sm dark:text-gray-300 px-4 pb-2 pt-1">
+            <p>
+              Retired from corporate and full time YouTuber, x founder of LCO
+              (acquired), x CTO, Sr. Director at PW. 2 YT channels (950k &
+              470k), stepped into 43 countries.
+            </p>
+
+            <div className="mt-2 flex items-center gap-4 text-sm dark:text-gray-400">
+              <span>
+                <span className=" font-semibold">386</span> Following
+              </span>
+              <span>
+                <span className=" font-semibold">21.6K</span> Followers
+              </span>
+            </div>
+          </CardContent>
+        </Card>
+
+        <hr className="my-12" />
+        <Card className="mt-6 rounded-2xl shadow-lg w-full max-w-xl mx-auto">
+          <CardHeader className="flex flex-col sm:flex-row sm:justify-between sm:items-center gap-4 px-4 pt-4">
+            <div className="flex items-center gap-3">
+              <Avatar url="/hitesh-sir.png" alt="avatar" />
+              <div>
+                <CardTitle className="text-base">Hitesh Chaudhary</CardTitle>
+                <CardDescription className="text-sm text-zinc-600 dark:text-zinc-400">
+                  @HiteshDotCom
+                </CardDescription>
+              </div>
+            </div>
+            <Button
+              onClick={clickHandler}
+              className={cn(
+                status === 'Unfollow'
+                  ? 'bg-zinc-900 hover:bg-neutral-900 dark:border dark:border-white border-2'
+                  : 'bg-blue-600 hover:bg-blue-700',
+                'transition-all duration-200 ease-out active:scale-95 text-white text-sm font-medium py-1.5 px-4 rounded-full'
+              )}
+            >
+              {status}
+            </Button>
+          </CardHeader>
+
+          <CardContent className="px-4 pb-4 pt-2 text-sm text-zinc-700 dark:text-zinc-300">
+            <p>
+              Retired from corporate and full time YouTuber, x founder of LCO
+              (acquired), x CTO, Sr. Director at PW. 2 YT channels (950k &
+              470k), stepped into 43 countries.
+            </p>
+
+            <div className="mt-3 flex flex-wrap gap-4 text-sm text-zinc-600 dark:text-zinc-400">
+              <span>
+                <span className="font-semibold">386</span> Following
+              </span>
+              <span>
+                <span className="font-semibold">21.6K</span> Followers
+              </span>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </div>
   );
 }
+
+export default TestCards;
